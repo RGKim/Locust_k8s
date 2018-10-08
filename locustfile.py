d241e83e62ecb952b1243919b7a540b03a3c46cb
SaeYoung Kim <heehouse1@gmail.com>
2018-10-08 18:26:07 +0900

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,43 +1,28 @@
 import base64
-import string
-import random
-import json
-import requests
  
-from locust import HttpLocust, TaskSet, task, seq_task
-from random import choice
+from locust import HttpLocust, TaskSet, task
+from random import randint, choice
  
  
 class WebTasks(TaskSet):
  
-    @seq_task(1)
+    @task
     def load(self):
+        base64_str = base64.encodestring(('%s:%s' % ('user','password')).encode()).decode().replace('\n', '')
+        catalogue = self.client.get("/catalogue").json()
+        category_item = choice(catalogue)
+        item_id = category_item["id"]
+ 
         self.client.get("/")
-
-        global username
-        username = ""
-        for _ in range(10):
-            username += str(random.choice(string.ascii_letters))
-        c_id = json.loads(self.client.post("/register", json={"username": username, "password": 'password'}).content)['id']
-        self.client.post("/cards", json={"longNum":"123123", "expires":"2310", "ccv":"456", "userID": c_id})
-        self.client.post("/addresses", json={"street":"123123", "number":"321321", "country":"Korea", "city": "Seongnam-si", "postcode": "123123", "userID": c_id})
-
-        base64_str = base64.encodestring(('%s:%s' % (username,'password')).encode()).decode().replace('\n', '')
         self.client.get("/login", headers={"Authorization":"Basic %s" % base64_str})
         self.client.get("/category.html")
-
-        global item_id
-        item_id = choice(self.client.get("/catalogue").json())["id"]
         self.client.get("/detail.html?id={}".format(item_id))
-  
-        if not self.client.get("/cart").content:
-            self.client.delete("/cart")
+        self.client.delete("/cart")
         self.client.post("/cart", json={"id": item_id, "quantity": 1})
-
-        if not self.client.get("/basket.html").content:
-            self.client.post("/orders", json={}) 
-
-
+        self.client.get("/basket.html")
+        self.client.post("/orders")
+ 
+ 
 class Web(HttpLocust):
     task_set = WebTasks
     min_wait = 500
